594085abda96314c36504ef69196040790b0272f
surajTricon <suraj.kumar@triconinfotech.com>
2024-09-03 13:00:45 +0530

    Streams

---
+++ StudentSorting.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class StudentSorting {
+    public static <T> void sortStudents(ArrayList<Student> students, Comparator<T> comparator) {
+    }
+    public static void main(String[] args) {
+            ArrayList<Student> students = new ArrayList<>();
+            students.add(new Student("Suraj", 1, D, 20));
+            students.add(new Student("Rahul", 2, B, 22));
+            students.add(new Student("Surya", 3, A, 21));
+
+
+    }
+}
+
